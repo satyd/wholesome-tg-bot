92760fc33394c9e29f0ccc8a59539967a525b812
satyd <satyd@mail.ru>
2021-05-08 21:03:49 +0300

    initial commit kek

---
+++ bot.py
@@ -0,0 +1,30 @@
+import telebot
+import random, os
+import datetime
+import pytz
+
+
+# TOKEN ='1756123183:AAFjCiddoo8IiWy4xrqq7ebFrZcuzEYbbLw'
+TIMEZONE = 'Europe/Kiev'
+
+P_TIMEZONE = pytz.timezone(TIMEZONE)
+
+# bot = telebot.TeleBot(TOKEN)
+
+
+
+@bot.message_handler(commands=['start','pic'])
+def get_text_messages(message):
+    if message.text == "/start":
+        bot.send_message(message.from_user.id, "to get wholesome, type /pic")
+    elif message.text == "/pic":
+        directory = "D:\z pic folder"
+        random_image = random.choice(os.listdir(directory))
+        with open ("D:\z pic folder"+"\\"+ random_image, "rb") as file:
+            bot.send_photo(message.from_user.id, photo = file)
+    else:
+        bot.send_message(message.from_user.id, ':/')
+            
+    
+        
+bot.polling(none_stop=True)
