f017b6314cf3a6d3388cd5dd25d39c6c19f05a77
satyd <satyd@mail.ru>
2021-05-19 23:42:44 +0300

    'f'

--- bot.py
+++ bot.py
@@ -13,18 +13,27 @@ bot = telebot.TeleBot(TOKEN)
 
 
 
-@bot.message_handler(commands=['start','pic'])
+@bot.message_handler(commands=['start','pic','meme','cursed'])
 def get_text_messages(message):
     if message.text == "/start":
-        bot.send_message(message.from_user.id, "to get wholesome, type /pic")
+        bot.send_message(message.from_user.id, "to get wholesome smth, type /pic\n/meme is for memes")
     elif message.text == "/pic":
-        #bot.send_message(message.from_user.id, "no pic :(")
         directory = "!!pics"
         random_image = random.choice(os.listdir(directory))
         with open ("!!pics"+"/"+ random_image, "rb") as file:
             bot.send_photo(message.from_user.id, photo = file)
+    elif message.text == "/meme":
+        directory = "!!memes"
+        random_image = random.choice(os.listdir(directory))
+        with open ("!!memes"+"/"+ random_image, "rb") as file:
+            bot.send_photo(message.from_user.id, photo = file)
+    elif message.text == "/cursed":
+    directory = "!!cursed"
+    random_image = random.choice(os.listdir(directory))
+    with open ("!!cursed"+"/"+ random_image, "rb") as file:
+        bot.send_photo(message.from_user.id, photo = file)
     else:
-        bot.send_message(message.from_user.id, ':/')
+        bot.send_message(message.from_user.id, 'no :/')
             
     
         
